e8e465ddad2a7f44fa51c4863b6cc58527265440
jennetkim <wjminjoo49@gmail.com>
2021-04-19 14:23:56 +0900

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
 import React from "react";
 import "./style.css";
 
-export default function page_1(){
+export default function App() {
   return (
     <div>
       <h1>실험 테스트입니다.</h1>
-
-
       <p>안녕하세요 UX 사용성 실험에 참가해주셔서 감사합니다. </p>
       <img src="https://miro.medium.com/max/1200/1*Fq1ITNx3AaF51lHVqAwjUQ.png" />
-
-
       <p> 다음의 버튼을 누르시면 실험이 시작됩니다. </p>
-      <button>
-        {" "}
-        <button1> 시작! </button1>{" "}
-      </button>
+      <input
+        id="go_home"
+        type="button"
+        onclick="newPage()"
+        value=" 다음 페이지로 이동"
+      />
+      function newPage() {(location.href = "page2.html")}
     </div>
   );
 }
-/*
-export default function page_2() {
-  return (
-    //버튼 누르고 시작되는 평가 페이지.... 
-    <div>
 
-    </div>
-  );
+/*
+버튼을 누르면 다음 페이지로 이동 
+--> <input type="button" value="확인" onClick="self.location='이동할페이지';">
+
+function page_href() {
+
+location.href = "link_page.html";
+
 }
+
+document.loaction.href = "";
 
 
 function execTime() {
